a0f77ea6f5dac7b6cbd41a595141a60f3ff24241
nessim9898 <nessime.baraket@etudiant-isi.utm.tn>
2024-09-22 23:14:09 +0100

    First commit

--- Rendue1/ZooManagement.java
+++ Rendue1/ZooManagement.java
@@ -9,13 +9,24 @@ public class ZooManagement
         System.out.println("Welcome to the Zoo Management System");
         int nbrCages=20;
         String zooName="my zoo";
-        System.out.println("Zoo Name : "+zooName + " Number of cages : "+nbrCages);
+        System.out.println("Zoo Name : "+zooName + "     Number of cages : "+nbrCages);
         Scanner scanner = new Scanner(System.in);
+        do{
         System.out.print("Enter the zoo name: ");
         zooName = scanner.nextLine();
-        System.out.print("Enter the number of cages: ");
-        nbrCages = scanner.nextInt();
-        scanner.nextLine();
+        } while (zooName.length() < 3);
+        do {
+            System.out.print("Enter the number of cages: ");
+            String nbrCagesStr = scanner.nextLine();
+            if (nbrCagesStr.matches("\\d+")) {
+                nbrCages = Integer.parseInt(nbrCagesStr);
+                break;
+            } else {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        } while (true);
+        
+        
         System.out.println("Zoo Name: " + zooName);
         System.out.println("Number of Cages: " + nbrCages);
     }
