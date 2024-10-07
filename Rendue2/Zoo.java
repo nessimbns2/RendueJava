91e1b2e9a26712558ee34fa2fb5dced54dcd9746
nessim9898 <nessime.baraket@etudiant-isi.utm.tn>
2024-10-07 11:00:57 +0100

    first commit

--- Rendue2/Zoo.java
+++ Rendue2/Zoo.java
@@ -1,16 +1,18 @@
 package Rendue2;
+import java.util.List;
 public class Zoo {
     public Animal[] animals;
     public String name;
     public String city;
-    public int nbrCages;
+    final int nbrCages= 25;
+    public int nbrAnimal;
 
 
-    public Zoo(String name, String city, int nbrCages, Animal[] animals) {
+    public Zoo(String name, String city, Animal[] animals) {
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
         this.animals = animals;
+        nbrAnimal = 0;
     }
     @Override
     public String toString() {
@@ -24,13 +26,73 @@ public class Zoo {
         System.out.println("City: " + city);
         System.out.println("Number of cages: " + nbrCages);
         System.out.println("Animals in the zoo: ");
-        for (Animal animal : animals) {
-            System.out.println("Name: " + animal.name);
-            System.out.println("Family: " + animal.family);
-            System.out.println("Age: " + animal.age);
-            System.out.println("Is Mammal: " + animal.isMammal);
+        for (int i = 0; i < this.nbrAnimal; i++) {
+            
+            System.out.println("Name: " + this.animals[i].name);
+            System.out.println("Family: " + this.animals[i].family);
+            System.out.println("Age: " + this.animals[i].age);
+            System.out.println("Is Mammal: " + this.animals[i].isMammal);
+            System.out.println(i);
         }
     }
+
+public boolean addAnimal(Animal animal){
+    if (nbrAnimal >= animals.length && searchAnimal(animal) != -1  && nbrAnimal >= nbrCages){
+        return false;
+    }
+    animals[nbrAnimal] = animal;
+    nbrAnimal++;
+        return true;
+    
+}
+
+public void displayAnimals(){
+    for (int i = 0; i < this.nbrAnimal; i++) {
+        System.out.println("Name: " + this.animals[i].name);
+        System.out.println("Family: " + this.animals[i].family);
+        System.out.println("Age: " + this.animals[i].age);
+        System.out.println("Is Mammal: " + this.animals[i].isMammal);
+    }
+}
+public int searchAnimal(Animal animal){
+    for (int i = 0; i < animals.length; i++) {
+        if (animals[i].name.equals(animal.name)){
+            return i;
+        }
+    }
+    return -1;
+}
+
+public boolean removeAnimal(Animal animal){
+    int index = searchAnimal(animal);
+    if (index == -1){
+        return false;
+    }
+    for (int i = index; i < nbrAnimal - 1; i++) {
+        animals[i] = animals[i + 1];
+    }
+    nbrAnimal--;
+    return true;}
+
+
+    public boolean isZooFull(){
+        return nbrAnimal == nbrCages;
+    }
+
+    public Zoo compareZoo(Zoo zoo1 , Zoo zoo2){
+        if (zoo1.nbrAnimal > zoo2.nbrAnimal){
+            return zoo1;
+        }
+        else if    (zoo1.nbrAnimal == zoo2.nbrAnimal){
+            return null;
+        }
+        return zoo2;
+    }
+
+
+
+
+
 
 
 }
--- Rendue2/ZooManagement.java
+++ Rendue2/ZooManagement.java
@@ -1,13 +1,24 @@
 package Rendue2;
 
+import java.util.*;
+
 public class ZooManagement {
     public static void main(String[] args) {
         System.out.println("Welcome to Zoo Management System");
         Animal lion = new Animal("Felidae", "Lion", 5, true);
-        Animal[] animals = {lion};
-        Zoo zoo = new Zoo("Zoo", "New York", 10, animals);
+        Animal[] animals = new Animal[25];
+       
         System.out.println(lion);    
+        Zoo zoo = new Zoo("Zoo", "New York",  animals);
+        zoo.addAnimal(lion);
         System.out.println(zoo.toString());
-       // zoo.displayZoo();
+        zoo.displayZoo();
+        
+        Animal elephant = new Animal("Elephantidae", "Elephant", 10, true);
+        
+        zoo.addAnimal(elephant);
+     
+        zoo.displayAnimals();
+        
     }
 }
